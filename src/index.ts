f935c08977c064cfd5acfb1979163e000a07ed75
Danilo Diez <daniloo10.dd@gmail.com>
2023-09-17 12:42:45 -0300

    first tests with the endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const addPlugin = new Elysia()
+  .state('new-todo', ({ params })=> params)
+  .get("/add-todo/:text", ({ params }) => params);
+
+const app = new Elysia()
+  .use(addPlugin)
+  .get("/caca", ({ store }) => store)
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
